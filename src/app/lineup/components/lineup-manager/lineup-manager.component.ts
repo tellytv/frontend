7b8bc164244721ccf9efd0d7e726ce42a3201f41
Guy Spronck <guy@sprofix.com>
2018-08-24 23:16:06 +0200

    Update lineup-manager design

--- src/app/lineup/components/lineup-manager/lineup-manager.component.ts
+++ src/app/lineup/components/lineup-manager/lineup-manager.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { GuideSourceService } from '@app/lineup/services/guide-source.service';
 import { VideoSourceService } from '@app/lineup/services/video-source.service';
@@ -10,33 +10,64 @@ import { GuideSourceChannel } from '@app/lineup/models/guide-source-channel.mode
 import { VideoSource } from '@app/lineup/models/video-source.model';
 import { VideoSourceTrack } from '@app/lineup/models/video-source-track.model';
 import { Observable } from 'rxjs/internal/Observable';
+import { DragulaService } from 'ng2-dragula';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-lineup-manager',
   templateUrl: './lineup-manager.component.html',
   styleUrls: ['./lineup-manager.component.scss']
 })
-export class LineupManagerComponent implements OnInit {
+export class LineupManagerComponent implements OnInit, OnDestroy {
+  DRAGULA_NAME = 'LINES';
+
   lineup$: Observable<Lineup>;
   guideChannels$: Observable<GuideSourceChannel[]>;
   videoTracks$: Observable<VideoSourceTrack[]>;
   newLineupChannel: Observable<LineupChannel> = new Observable<LineupChannel>();
 
+  lineup: Lineup;
+
+  editingChannel: LineupChannel;
+
+  subs = new Subscription();
+
   constructor(
     private lineupService: LineupService,
     private guideSourceService: GuideSourceService,
     private videoSourceService: VideoSourceService,
+    private dragulaService: DragulaService
   ) { }
 
   ngOnInit(): void {
     this.lineup$ = this.lineupService.getLineup(1);
     this.guideChannels$ = this.guideSourceService.getAllChannels();
     this.videoTracks$ = this.videoSourceService.getAllTracks();
+
+    this.lineup$.subscribe((lineup) => {
+      this.lineup = lineup;
+    });
+
+    this.subs.add(this.dragulaService.drop(this.DRAGULA_NAME).subscribe(() => {
+      this.updateChannelNumbers(this.lineup.Channels);
+      this.updateChannelNumbers(this.lineup.DisabledChannels);
+    }));
+  }
+
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
   }
 
   addChannelToLineup(): void {
     // TODO: Add channel
     this.newLineupChannel = null;
+  }
+
+  updateChannelNumbers(channels: LineupChannel[]): void {
+    for (let i = 0; i < channels.length; i++) {
+      const channelNumber = i + 1;
+      channels[i].ChannelNumber = `${channelNumber}`;
+    }
   }
 
 }
